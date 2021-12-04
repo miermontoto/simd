1475a2c5c6df7a6d21ef54a1c1b75bca09ccb723
UO283319 <UO283319@uniovi.es>
2021-12-04 03:38:01 +0100

    estructura de lo que debería ser el algoritmo

--- pruebasimd.cpp
+++ pruebasimd.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 using namespace cimg_library;
 
-#define repetitions 17
-#define itemsPerPacket (sizeof(__m256)/sizeof(float))
+#define REPETITIONS 17
+#define ITEMSPERPACKET (sizeof(__m256) / sizeof(float))
 
 const char* SOURCE_IMG      = "bailarina.bmp"; // source image's file name.
 const char* HELP_IMG        = "background_V.bmp"; // aid image's file name.
@@ -27,6 +27,7 @@ int main() {
     float *pDstImage;
     uint width, height; // General image information variables
 	uint nComp;
+    int nPixels = width * height;
 
     // Time variables necessary to display the execution time when the algorithm is done.
 	struct timespec tStart, tEnd;
@@ -38,16 +39,16 @@ int main() {
 	nComp  = srcImage.spectrum();
 
     // Resulting image allocation
-    pDstImage = (float *) malloc (width * height * nComp * sizeof(float));
+    pDstImage = (float *) malloc (nPixels * nComp * sizeof(float));
 	if (pDstImage == NULL) {
 		perror("Couldn't allocate resulting image.");
 		exit(1);
 	}
 
     // Number of packets necessary to process an image.
-    int nPackets = ((width * height) / itemsPerPacket);
+    int nPackets = (nPixels / ITEMSPERPACKET);
     // If it isn't divisible, add one more packet (it won't be completely processed)
-    if ( ((width * height * sizeof(float)) % sizeof(__m256)) != 0) nPackets++;
+    if ( ((nPixels * sizeof(float)) % sizeof(__m256)) != 0) nPackets++;
 
     // Memory allocation for the destination image's components.
     float *pRdest = (float *)_mm_malloc(sizeof(__m256) * nPackets, sizeof(__m256));
@@ -57,33 +58,62 @@ int main() {
     // 256bit packets are created in order to store those components in memory.
     __m256 vR, vG, vB; 
 
+
+    // !!! no estoy del todo seguro de que esto sea necesario, se debería trabajar por
+    // !!! paquetes DENTRO del propio algoritmo y cada paquete se genera dentro, por lo
+    // !!! que igualar a -1 no tiene mucho sentido. aunque también podría estar perfecto,
+    // !!! no tengo ni idea.
+
     // Every component is initialized as "-1".
     *(__m256 *) pRdest = _mm256_set1_ps(-1);
-    *(__m256 *)(pRdest + itemsPerPacket)     = _mm256_set1_ps(-1);
-    *(__m256 *)(pRdest + itemsPerPacket * 2) = _mm256_set1_ps(-1);
+    *(__m256 *)(pRdest + ITEMSPERPACKET)     = _mm256_set1_ps(-1);
+    *(__m256 *)(pRdest + ITEMSPERPACKET * 2) = _mm256_set1_ps(-1);
     *(__m256 *) pGdest = _mm256_set1_ps(-1);
-    *(__m256 *)(pGdest + itemsPerPacket)     = _mm256_set1_ps(-1);
-    *(__m256 *)(pGdest + itemsPerPacket * 2) = _mm256_set1_ps(-1);
+    *(__m256 *)(pGdest + ITEMSPERPACKET)     = _mm256_set1_ps(-1);
+    *(__m256 *)(pGdest + ITEMSPERPACKET * 2) = _mm256_set1_ps(-1);
     *(__m256 *) pBdest = _mm256_set1_ps(-1);
-    *(__m256 *)(pBdest + itemsPerPacket)     = _mm256_set1_ps(-1);
-    *(__m256 *)(pBdest + itemsPerPacket * 2) = _mm256_set1_ps(-1);
+    *(__m256 *)(pBdest + ITEMSPERPACKET)     = _mm256_set1_ps(-1);
+    *(__m256 *)(pBdest + ITEMSPERPACKET * 2) = _mm256_set1_ps(-1);
+    
+	// pointer initialization. same as in single-thread??
 
-    pRsrc = srcImage.data();        // componente roja
-	pGsrc = pRsrc + height * width; // componente verde
-	pBsrc = pGsrc + height * width; // componente azul
+	// source image pointers
+	pRsrc = srcImage.data();         // red component
+	pGsrc = pRsrc + height * width;  // green component
+	pBsrc = pGsrc + height * width;  // blue component
 
-	// Punteros a la imagen de apoyo
-	pRaid = aidImage.data();        // componente roja
-	pGaid = pRaid + height * width; // componente verde
-	pBaid = pGaid + height * width; // componente azul
+	// help image pointers
+	pRaid = aidImage.data();         // red component
+	pGaid = pRaid + height * width;  // green component
+	pBaid = pGaid + height * width;  // blue component
 
-	// Punteros a la imagen resultante
-	pRdest = pDstImage;               // componente roja
-	pGdest = pRdest + height * width; // componente verde
-	pBdest = pGdest + height * width; // componente azul
+	// destination image pointers
+	pRdest = pDstImage;               // red component
+	pGdest = pRdest + height * width; // green component
+	pBdest = pGdest + height * width; // blue component
 
-    for(int i = 0; i < repetitions; i++) {
-        // el algoritmo traducido va aquí <-
+    for(int i = 0; i < REPETITIONS; i++) {
+
+        // Packets for each image are initialized.
+        __m256 kRsrc, kGsrc, kBsrc;
+        __m256 kRaid, kGaid, kBaid;
+        __m256 kRdest, kGdest, kBdest;
+
+        // The algorithm should operate with EACH block of pixels, each one of them being
+        // 'itemsPerPacket' in size.
+        for(int k = 0; k < nPixels; k += ITEMSPERPACKET) {
+
+            // aquí se lee un paquete
+            // aquí se convierte un paquete de alguna manera a floats???
+            // se hace lo que se hacía en single-thread para operar con los floats.
+            // min max para evitar valores inváldos...
+            // se guardan los resultados??? no sé cómo
+
+            for(int j = 0; j < ITEMSPERPACKET; j++) {
+                // para cada float, algo. no tengo ni idea.
+            }
+
+        }
     }
 
     if (clock_gettime(CLOCK_REALTIME, &tStart)==-1) {
